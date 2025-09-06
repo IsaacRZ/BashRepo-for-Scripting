2bd96f71f0fe13890461b9830257f6844110c402
Isaac <isaac_024@hotmail.com>
2025-09-06 17:12:16 -0600

    Mi rama de prueba

--- bienvenido.sh
+++ bienvenido.sh
@@ -1,10 +1,12 @@
 NOMBRE="Isaac"
 HORA=$(date +%H)
 
-if [$HORA -ge 6] && [$HORA -lt 12]; then
+if [ $HORA -ge 6 ] && [ $HORA -lt 12 ]; then
   echo "Buenos dias $Nombre, la hora actual es: $(date +%H:%M)"
-elif [$HORA -ge 12] && [$HORA -lt 18]; then
+elif [ $HORA -ge 12 ] && [ $HORA -lt 18 ]; then
   echo "Buenos tardes $Nombre, la hora actual es: $(date +%H:%M)"
 else
   echo "Buenos noches $Nombre, la hora actual es: $(date +%H:%M)"
 fi
+
+echo "Mi rama de prueba"
